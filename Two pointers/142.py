8114d556c486aa5f9897ac3c026c8ca824e7c8b5
jsheng_b20a <jsheng0901@gmail.com>
2023-06-26 13:21:40 -0400

    update

--- Two pointers/142.py
+++ Two pointers/142.py
@@ -4,7 +4,7 @@ class ListNode:
         self.next = next
 
 
-def detectCycle(head: ListNode) -> ListNode:
+def detectCycle1(head: ListNode) -> ListNode:
     fast = head
     slow = head
     while fast and fast.next:
@@ -22,6 +22,29 @@ def detectCycle(head: ListNode) -> ListNode:
     return -1
 
 
+def detectCycle2(head: ListNode) -> ListNode:
+    fast = head
+    slow = head
+
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow:
+            break
+
+    if fast is None or fast.next is None:
+        return None
+
+    index1 = head
+    index2 = fast
+
+    while index1 != index2:
+        index1 = index1.next
+        index2 = index2.next
+
+    return index1
+
+
 l1 = ListNode(1)
 l2 = ListNode(2)
 l3 = ListNode(3)
@@ -30,4 +53,4 @@ l1.next = l2
 l2.next = l3
 l3.next = l4
 l4.next = l2
-print(detectCycle(l1).val)
+print(detectCycle1(l1).val)
--- Two pointers/160.py
+++ Two pointers/160.py
@@ -5,7 +5,11 @@ class ListNode:
 
 
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+    def getIntersectionNode1(self, headA: ListNode, headB: ListNode) -> ListNode:
+        """
+        time: O(n)
+        space: O(1)
+        """
         p1 = headA
         p2 = headB
 
@@ -21,3 +25,48 @@ class Solution:
                 p2 = headA
 
         return p1
+
+    def getIntersectionNode2(self, headA: ListNode, headB: ListNode) -> ListNode:
+        """
+        time: O(n)
+        space: O(1)
+        先找到两个链表的长度，然后让长的链表走到和短链表的相同位置，
+        然后同时两个链表一起走check是否有相同node，没有则返回None，有则返回node
+        """
+        p1 = headA
+        p2 = headB
+        l_a = 0
+        l_b = 0
+
+        while p1:
+            p1 = p1.next
+            l_a += 1
+
+        while p2:
+            p2 = p2.next
+            l_b += 1
+
+        new_p1 = headA
+        new_p2 = headB
+        if l_a >= l_b:
+            diff = l_a - l_b
+            while diff > 0:
+                new_p1 = new_p1.next
+                diff -= 1
+
+            while new_p1 != new_p2 and new_p1 is not None and new_p2 is not None:
+                new_p1 = new_p1.next
+                new_p2 = new_p2.next
+
+            return new_p1 if new_p1 is not None else None
+        else:
+            diff = l_b - l_a
+            while diff > 0:
+                new_p2 = new_p2.next
+                diff -= 1
+
+            while new_p1 != new_p2 and new_p1 is not None and new_p2 is not None:
+                new_p1 = new_p1.next
+                new_p2 = new_p2.next
+
+            return new_p2 if new_p2 is not None else None
