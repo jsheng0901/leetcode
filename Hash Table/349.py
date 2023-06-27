461589c6f41b19fa29efdebf2500def53f6abfd6
jsheng_b20a <jsheng0901@gmail.com>
2023-06-27 14:16:39 -0400

    update

--- Hash Table/349.py
+++ Hash Table/349.py
@@ -1,15 +1,19 @@
 class Solution:
     def intersection(self, nums1: [int], nums2: [int]) -> [int]:
+        """
+        time: O(n) n = len(nums1) + len(nums2)
+        space: O(n) n = len(result)
+        """
         # 存放结果
-        resutl = []
+        result = []
 
         record = set(nums1)
 
         for i in set(nums2):
             if i in record:
-                resutl.append(i)
+                result.append(i)
 
-        return resutl
+        return result
 
 
 s = Solution()
