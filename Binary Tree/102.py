effbb1f9f9383f62f25f7bd4f2b62c7dac166099
jsheng_b20a <jsheng0901@gmail.com>
2023-07-07 15:34:26 -0400

    update

--- Binary Tree/102.py
+++ Binary Tree/102.py
@@ -10,22 +10,22 @@ def levelOrder(root: TreeNode) -> [[int]]:
     """
     层序遍历，及一层一层的读取二叉树的数值，从左到右过一遍记录下来
     """
-    quene = []
+    queue = []
     if root is not None:
-        quene.append(root)
+        queue.append(root)
 
     results = []
-    while len(quene) > 0:
-        size = len(quene)
+    while len(queue) > 0:
+        size = len(queue)
         vector = []
         # 这里一定要使用固定大小size，不要使用que.size()，因为que.size是不断变化的
         for i in range(size):     # loop每一层的tree node
-            front_node = quene.pop(0)
+            front_node = queue.pop(0)
             vector.append(front_node.val)
             if front_node.left:
-                quene.append(front_node.left)
+                queue.append(front_node.left)
             if front_node.right:
-                quene.append(front_node.right)
+                queue.append(front_node.right)
 
         results.append(vector)
 
