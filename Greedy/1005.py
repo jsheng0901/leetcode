0ee7c66d8a256e9d4566a200705e3cb417ae8dd2
jsheng_b20a <jsheng0901@gmail.com>
2023-07-28 12:23:11 -0400

    update

--- Greedy/1005.py
+++ Greedy/1005.py
@@ -1,29 +1,30 @@
 class Solution:
-    def largestSumAfterKNegations(self, A: [int], K: int) -> int:
+    def largestSumAfterKNegations(self, nums: [int], k: int) -> int:
         """
-        第一步：将数组按照绝对值大小从大到小排序，「注意要按照绝对值的大小」
+        Time O(n)
+        Space O(n) sorted会产生新的数组copy
+        第一步：将数组按照绝对值大小从大到小排序，注意要按照绝对值的大小
         第二步：从前向后遍历，遇到负数将其变为正数，同时K--
         第三步：如果K还大于0，那么反复转变数值最小的元素，将K用完
         第四步：求和
-        :param A:
-        :param K:
-        :return:
         """
-        A = sorted(A, key=abs)  # 第一步
-        A = A[::-1]
-        for i in range(len(A)):
-            if A[i] < 0 and K > 0:
-                A[i] *= -1
-                K -= 1
+        nums = sorted(nums, key=abs, reverse=True)  # 第一步
+        for i in range(len(nums)):
+            if k > 0 > nums[i]:
+                nums[i] *= -1
+                k -= 1
 
-        while K > 0:
-            A[-1] *= -1
-            K -= 1
+        # while k > 0:
+        #     nums[-1] *= -1
+        #     k -= 1
+        # 直接判断奇数或者偶数剩下的k
+        if k % 2 == 1:
+            nums[-1] *= -1
 
-        result = sum(A)
+        result = sum(nums)
 
         return result
 
 
 s = Solution()
-print(s.largestSumAfterKNegations(A=[4, 2, 3], K=1))
+print(s.largestSumAfterKNegations(nums=[4, 2, 3], k=1))
