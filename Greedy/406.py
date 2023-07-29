c0615eddc6617dcfa96a5af683b6e29f589953c1
jsheng_b20a <jsheng0901@gmail.com>
2023-07-29 14:54:13 -0400

    update

--- Greedy/406.py
+++ Greedy/406.py
@@ -4,28 +4,27 @@ import functools
 class Solution:
     def reconstructQueue(self, people: [[int]]) -> [[int]]:
         """
-        time: O(nlogn + n^3), space: O(n)
+        Time: O(nlogn + n^3)
+        Space: O(n)
         数组的插入操作复杂度是O(n^2)：寻找插入元素位置O(1)，插入元素O(n^2)，因为插入元素后面的元素要整体向后移。n个数所以是n^3
-        我们就是要模拟一个插入队列的行为，所以不应该使用数组，而是要使用链表！
+        我们就是要模拟一个插入队列的行为，所以不应该使用数组，而是要使用链表！但python貌似不能这么做这题。
 
         遇到两个维度权衡的时候，一定要先确定一个维度，再确定另一个维度。
         此题用h先排序，这样按身高高度从大到小先确定，同身高，K小的在前面，
         按照身高排序之后，优先按身高高的people的k来插入，后序插入节点也不会影响前面已经插入的节点，最终按照k的规则完成了队列。
 
-        「局部最优：优先按身高高的people的k来插入。插入操作过后的people满足队列属性」
-「       全局最优：最后都做完插入操作，整个队列满足题目队列属性」
-        :param people:
-        :return:
+        局部最优：优先按身高高的people的k来插入。插入操作过后的people满足队列属性
+        全局最优：最后都做完插入操作，整个队列满足题目队列属性」
         """
 
         def cmp(item1, item2):
             if item1[0] == item2[0]:
-                if item1[1] < item2[1]:
+                if item1[1] < item2[1]:     # position更小的排在前面，所以return -1，表示less than
                     return -1
                 else:
                     return 1
             else:
-                if item1[0] < item2[0]:
+                if item1[0] < item2[0]:     # 因为是升序排列，所以return 1，值更小的反而表示greater than
                     return 1
                 else:
                     return -1
--- Greedy/860.py
+++ Greedy/860.py
@@ -1,13 +1,14 @@
+from typing import List
+
+
 class Solution:
-    def lemonadeChange(self, bills: [int]) -> bool:
+    def lemonadeChange1(self, bills: [int]) -> bool:
         """
         有如下三种情况：
             情况一：账单是5，直接收下。
             情况二：账单是10，消耗一个5，增加一个10
             情况三：账单是20，优先消耗一个10和一个5，如果不够，再消耗三个5
         局部最优：遇到账单20，优先消耗美元10，完成本次找零。全局最优：完成全部账单的找零。
-        :param bills:
-        :return:
         """
         five, ten, twenty = 0, 0, 0
 
@@ -36,8 +37,39 @@ class Solution:
 
         return True
 
+    def lemonadeChange2(self, bills: List[int]) -> bool:
+        """
+        另一种写法，通过找零来判断是否有bill
+        """
+        five, ten, twenty = 0, 0, 0
+        for bill in bills:
+            charge = bill - 5
+            if charge == 0:
+                five += 1
+            elif charge == 5:
+                ten += 1
+                if five <= 0:
+                    return False
+                else:
+                    five -= 1
+            elif charge == 15:
+                twenty += 1
+                if ten >= 1:
+                    ten -= 1
+                    if five >= 1:
+                        five -= 1
+                    else:
+                        return False
+                else:
+                    if five >= 3:
+                        five -= 3
+                    else:
+                        return False
+
+        return True
+
 
 s = Solution()
-print(s.lemonadeChange(bills=[5, 5, 5, 10]))
+print(s.lemonadeChange1(bills=[5, 5, 5, 10]))
 
 
