3a7e0987fe0e613f72474120b4dfb23dfd87e6c4
jsheng_b20a <jsheng0901@gmail.com>
2023-08-07 11:25:12 -0400

    update

--- Dynamic Programming/122.py
+++ Dynamic Programming/122.py
@@ -1,14 +1,15 @@
 class Solution:
     def maxProfit(self, prices: [int]) -> int:
         """
+        Time O(n)
+        Space O(n)
         和121一样，只是计算持有股票的时候要考虑之前的盈利情况
-        O(n) time, O(n) space
         """
         dp = [[0, 0] for i in range(len(prices))]
         dp[0][0] = -prices[0]
 
         for i in range(1, len(prices)):
-            dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i])
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i])      # 考虑之前的盈利情况dp[i-1][1]
             dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])
 
         return dp[-1][1]
