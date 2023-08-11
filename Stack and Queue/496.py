ac541625ea14c2266691ea09c2f573f426310b3a
junzhi sheng <jsheng0901@gmail.com>
2023-08-11 12:25:14 -0400

    update

--- Stack and Queue/496.py
+++ Stack and Queue/496.py
@@ -1,7 +1,8 @@
 class Solution:
-    def nextGreaterElement(self, nums1: [int], nums2: [int]) -> [int]:
+    def nextGreaterElement1(self, nums1: [int], nums2: [int]) -> [int]:
         """
-        O(n) time, O(n) space
+        Time O(n)
+        Space O(n)
         单调递增(stack top -> stack bottom)栈, 当当前数字大于栈顶数字的时候，判断栈顶数字是否是nums1里面的数字，如果是
         则说明找到了第一个比它大的数字，此时找到nums1的index并且记录当前数字到这个index去，弹出栈顶数字，继续loop nums2
         """
@@ -22,6 +23,26 @@ class Solution:
 
         return result
 
+    def nextGreaterElement2(self, nums1: [int], nums2: [int]) -> [int]:
+        """
+        另一种写法，不需要初始化stack
+        """
+        result = [-1] * len(nums1)
+        stack = []
+        nums1_map = {}
+        for i in range(len(nums1)):
+            nums1_map[nums1[i]] = i
+
+        for i in range(len(nums2)):
+            while len(stack) > 0 and nums2[stack[-1]] < nums2[i]:       # 判断是否当前数字大于stack top数字
+                top_index = stack.pop()
+                if nums2[top_index] in nums1_map:       # 判断此数字是否在nums1里面出现
+                    nums1_top_index = nums1_map[nums2[top_index]]
+                    result[nums1_top_index] = nums2[i]
+            stack.append(i)
+
+        return result
+
 
 s = Solution()
-print(s.nextGreaterElement(nums1=[4, 1, 2], nums2=[1, 3, 4, 2]))
+print(s.nextGreaterElement1(nums1=[4, 1, 2], nums2=[1, 3, 4, 2]))
--- Stack and Queue/739.py
+++ Stack and Queue/739.py
@@ -1,10 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: [int]) -> [int]:
         """
+        Time O(n)
+        Space O(n)
+        三种情况：
+            情况一：当前遍历的元素T[i]小于栈顶元素的情况
+            情况二：当前遍历的元素T[i]等于栈顶元素的情况
+            情况三：当前遍历的元素T[i]大于栈顶元素的情况
+        如果求一个元素右边第一个更大元素，单调栈就是递增的，如果求一个元素右边第一个更小元素，单调栈就是递减的。
+        这里的递增指的是从栈头到栈底的顺序，及加入数字大于stack[-1]的数字时候开始判断
         单调stack的应用，如果加入stack的数大于stack最后一个数，则弹出最后一个数，并记录此时的index差值到result，
         直到stack最后一个数大于新加入的数，停止loop，继续迭代
-        :param temperatures:
-        :return:
         """
         if len(temperatures) == 0:
             return []
@@ -13,11 +19,11 @@ class Solution:
         result = [0] * len(temperatures)
 
         for i in range(len(temperatures)):
-            while len(stack) > 0 and temperatures[stack[-1]] < temperatures[i]:
+            while len(stack) > 0 and temperatures[stack[-1]] < temperatures[i]:     # 对应情况3，注意判断栈不能为空
                 index = stack.pop()
                 result[index] = i - index
 
-            stack.append(i)     # 加入stack的是对应数的index
+            stack.append(i)     # 加入stack的是对应数的index，对应情况1，2
 
         return result
 
