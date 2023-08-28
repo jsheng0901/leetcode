2601dbb8c66d497398d661dcc2bee93f3d5df2d0
junzhi sheng <jsheng0901@gmail.com>
2023-08-28 16:00:31 -0400

    update

--- Dynamic Programming/63.py
+++ Dynamic Programming/63.py
@@ -1,4 +1,7 @@
-class Solution:
+from typing import List
+
+
+class Solution1:
     def uniquePathsWithObstacles(self, obstacleGrid: [[int]]) -> int:
         """
         Time: O(m*n)
@@ -38,5 +41,35 @@ class Solution:
         return dp[-1][-1]
 
 
-s = Solution()
+class Solution2:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        """
+        Time: O(m*n)
+        Space: O(m*n)
+        和第一种方法原理几乎一致。区别在于初始化第一行第一列可以放进整个loop一起，只需要初始化第一个点就行。
+        """
+
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        dp = [[0] * n for _ in range(m)]
+
+        # 只需要初始化起点，如果可以出发则1不能则0
+        dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    # 当初始化第一行的时候，如果不是障碍物，直接自己0+1，如果是障碍物，if直接设置为0
+                    if i > 0:
+                        dp[i][j] += dp[i - 1][j]
+                    # 同理初始化行
+                    if j > 0:
+                        dp[i][j] += dp[i][j - 1]
+
+        return dp[-1][-1]
+
+
+s = Solution1()
 print(s.uniquePathsWithObstacles(obstacleGrid=[[0, 0], [1, 1], [0, 0]]))
