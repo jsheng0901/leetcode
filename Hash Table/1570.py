fe29228e3daeeefd0ae55372b0c2c1d05e997a62
junzhi sheng <jsheng0901@gmail.com>
2023-11-08 22:40:02 -0500

    update

--- Hash Table/1570.py
+++ Hash Table/1570.py
@@ -1,4 +1,7 @@
-class SparseVector:
+from typing import List
+
+
+class SparseVector1:
     def __init__(self, nums: [int]):
         self.nonzeros = {}
         for i, n in enumerate(nums):
@@ -7,8 +10,9 @@ class SparseVector:
 
     def dotProduct(self, vec: 'SparseVector') -> int:
         """
+        Time O(n)
+        Space O(l)
         构建非0的字典，记录对应的index， 然后判断哪一个最短，loop这个字典，并记录同时index不是0的乘积，叠加进result
-        O(n) time, O(l) space
         """
         result = 0
         # iterate through each non-zero element in this sparse vector
@@ -25,7 +29,34 @@ class SparseVector:
                 result += n * max_dict[i]
         return result
 
-# Your SparseVector object will be instantiated and called as such:
-# v1 = SparseVector(nums1)
-# v2 = SparseVector(nums2)
-# ans = v1.dotProduct(v2)
+
+class SparseVector2:
+    def __init__(self, nums: List[int]):
+        self.no_zeor_index = set()
+        self.index_to_value = {}
+
+        for i, v in enumerate(nums):
+            if v != 0:
+                self.no_zeor_index.add(i)
+                self.index_to_value[i] = v
+
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        """
+        Time O(n)
+        Space O(l) * 2
+        构建非0的字典，记录index和value的关系，同时记录非0的index进set，
+        我们只需要找到非0的集合的交集即可，然后loop交集的index，叠加进result。这样减少loop的元素个数，更快。
+        """
+        res = 0
+        # 找交集
+        intersection_index = self.no_zeor_index.intersection(vec.no_zeor_index)
+        # 更新结果
+        for index in intersection_index:
+            res += self.index_to_value[index] * vec.index_to_value[index]
+
+        return res
+
+
+v1 = SparseVector2(nums=[1, 0, 0, 2, 3])
+v2 = SparseVector2(nums=[0, 3, 0, 4, 0])
+print(v1.dotProduct(v2))
--- Two pointers/680.py
+++ Two pointers/680.py
@@ -1,14 +1,15 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         """
-        左右指针判断，第一次不同的时候只动右指针，第二次不同的时候只动左指针，然后取最小值，判断是否小于等于1
+        Time O(2n) -> O(n)
+        Space O(1)
+        双指针指针判断，第一次loop遇到不同的时候只动右指针，第二次loop遇到不同的时候只动左指针，然后取最小值，判断是否小于等于1
         此题可以用dp方法找出最长回文子序列(subsequence)再用原始长度减去这个结果，判断是否大于一，不过会超时
-        dp: O(n^2)
-        two points: O(2n) --> O(n)
         """
         left = 0
         right = len(s) - 1
         count1 = 0
+        # 第一次loop
         while left < right:
             if s[left] == s[right]:
                 left += 1
@@ -16,12 +17,13 @@ class Solution:
             else:
                 count1 += 1
                 right -= 1
-        if count1 <= 1:     # 如果已经小于，则已经找到可行方案，直接返回
+        if count1 <= 1:  # 如果已经小于，则已经找到可行方案，直接返回
             return True
 
         left = 0
         right = len(s) - 1
         count2 = 0
+        # 第二次loop
         while left < right:
             if s[left] == s[right]:
                 left += 1
@@ -29,7 +31,44 @@ class Solution:
             else:
                 count2 += 1
                 left += 1
-                if count2 > 1:      # 如果已经大于则不可能，直接返回
+                if count2 > 1:  # 如果已经大于则不可能，直接返回
                     return False
 
-        return min(count1, count2) <= 1
+        # 只需要return第二次count是否等于1，因为如果第一次count等于1，则根本不会到第二次count
+        return count2 == 1
+
+
+class Solution2:
+    def check_palindrome(self, s, i, j):
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+
+        return True
+
+    def validPalindrome(self, s: str) -> bool:
+        """
+        Time O(n)
+        Space O(1)
+        同样的思路，只是当遇到不相等的字符的时候，直接判断左指针往前走一步，或者右指针往左跳一步，判断是否是回文，有一个是回文即可。
+        这里其实思路就等同于回溯的切割，找到一次切割的所有组合，如果新地组合是回文则回溯返回bool来判断是否有合理的一条path。
+        """
+
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            if s[i] != s[j]:
+                left = self.check_palindrome(s, i + 1, j)
+                right = self.check_palindrome(s, i, j - 1)
+                return left or right
+            i += 1
+            j -= 1
+
+        return True
+
+
+s = Solution2()
+print(s.validPalindrome(s="abca"))
