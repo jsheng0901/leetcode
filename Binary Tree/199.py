4d973f399e591c0e15051491a4a790c62f5653a0
junzhi sheng <jsheng0901@gmail.com>
2024-01-06 21:56:10 -0500

    update

--- Binary Tree/199.py
+++ Binary Tree/199.py
@@ -6,28 +6,31 @@ class TreeNode:
         self.right = right
 
 
-def rightSideView(root: TreeNode) -> [[int]]:
-    """
-    层序遍历，及一层一层的读取二叉树的数值，从左到右过一遍记录下来
-    """
-    quene = []
-    if root is not None:
-        quene.append(root)
+class Solution:
+    def rightSideView(self, root: TreeNode) -> [[int]]:
+        """
+        Time O(n)
+        Space O(n)
+        层序遍历，及一层一层的读取二叉树的数值，从左到右过一遍记录下来
+        """
+        queue = []
+        if root is not None:
+            queue.append(root)
 
-    results = []
-    while len(quene) > 0:
-        size = len(quene)
-        # 这里一定要使用固定大小size，不要使用que.size()，因为que.size是不断变化的
-        for i in range(size):           # loop每一层的tree node
-            front_node = quene.pop(0)
-            if i == size - 1:           # 这里判断一下此时的node是不是最右边及每一层最后一个node
-                results.append(front_node.val)
-            if front_node.left:
-                quene.append(front_node.left)
-            if front_node.right:
-                quene.append(front_node.right)
+        results = []
+        while len(queue) > 0:
+            size = len(queue)
+            # 这里一定要使用固定大小size，不要使用que.size()，因为que.size是不断变化的
+            for i in range(size):           # loop每一层的tree node
+                front_node = queue.pop(0)
+                if i == size - 1:           # 这里判断一下此时的node是不是最右边及每一层最后一个node
+                    results.append(front_node.val)
+                if front_node.left:
+                    queue.append(front_node.left)
+                if front_node.right:
+                    queue.append(front_node.right)
 
-    return results
+        return results
 
 
 t1 = TreeNode(val=1)
@@ -35,4 +38,5 @@ t2 = TreeNode(val=2)
 t3 = TreeNode(val=3)
 t1.left = t2
 t1.right = t3
-print(rightSideView(t1))
+s = Solution()
+print(s.rightSideView(t1))
--- Two pointers/162.py
+++ Two pointers/162.py
@@ -1,14 +1,27 @@
 class Solution:
     def findPeakElement(self, nums: [int]) -> int:
-        """双指针左右"""
+        """
+        Time O(log(n))
+        Space O(1)
+        双指针二分法变体，查找此点是不是在下降的趋势上，如果是下降的趋势，说明peak在左边并包含此点，如果不是下降的趋势，说明peak在右边。
+        """
         left = 0
         right = len(nums) - 1
 
         while left < right:
             mid = left + (right - left) // 2
+            # 下一个比当前的小，说明peak在左边包含当前点
             if nums[mid] > nums[mid + 1]:
                 right = mid
+            # 下一个比当前的大，说明peak在右边并且当前数字不可能是peak
             else:
                 left = mid + 1
 
+        # 出循序的时左指针表示peak
         return left
+
+
+s = Solution()
+print(s.findPeakElement(nums=[1, 2, 3]))
+print(s.findPeakElement(nums=[3, 2, 1]))
+print(s.findPeakElement(nums=[1, 2, 3, 4, 2]))
