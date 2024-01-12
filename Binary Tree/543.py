5d43e2711dc859675ffb2bf91c854ad65e1d7932
junzhi sheng <jsheng0901@gmail.com>
2024-01-11 22:36:42 -0500

    update

--- Binary Tree/543.py
+++ Binary Tree/543.py
@@ -14,21 +14,37 @@ class Solution:
         if node is None:
             return 0
 
+        # 左右子树对应的最大深度
         left = self.traversal(node.left, depth)
         right = self.traversal(node.right, depth)
 
+        # 当前节点的最大直径为左右子树的深度和
         if left + right > self.diameter:
             self.diameter = left + right
 
+        # 返回当前节点的最大深度
         return max(left, right) + 1
 
     def diameterOfBinaryTree(self, root: [TreeNode]) -> float:
         """
         Time O(n) 遍历所有node
         Space O(n) 递归占用
-        记录每个节点的最大深度，同时记录最大diameter
+        最大直径为左右子树的最大深度和，同时利用后续遍历返回左右子树的最大深度，记录每个节点的最大深度，同时记录最大diameter。
         """
         if root:
             self.traversal(root, 0)
 
         return self.diameter
+
+
+node1 = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(3)
+node4 = TreeNode(4)
+node5 = TreeNode(5)
+node1.left = node2
+node1.right = node3
+node2.left = node4
+node2.right = node5
+s = Solution()
+print(s.diameterOfBinaryTree(node1))
--- Hash Table/398.py
+++ Hash Table/398.py
@@ -1,25 +1,55 @@
 from collections import defaultdict
 import random
+from typing import List
 
 
-class Solution:
+class Solution1:
 
     def __init__(self, nums: [int]):
+        """
+        Time O(n)
+        Space O(n)
+        """
         self.mapping = defaultdict(list)
         for i in range(len(nums)):
             self.mapping[nums[i]].append(i)
 
     def pick(self, target: int) -> int:
         """
-        O(n) time initial, O(1) time pick, O(n) space
-        initial的时候构建hash map存储具体数字对应的index的list，pick的时候random选一个index
+        Time O(1)
+        Space O(1)
+        initial的时候构建hash map存储具体数字对应的index的list，pick的时候random选一个index。这里需要额外的空间来存储index。
         """
         index = self.mapping[target]
-        # if len(index) == 1:
-        #     return index[0]
-        # else:
         return random.choice(index)
 
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(nums)
-# param_1 = obj.pick(target)
+
+class Solution2:
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.rand = random.Random()
+
+    def pick(self, target: int) -> int:
+        """
+        Time O(n)
+        Space O(1)
+        不需要额外的空间来构建hash table，这里用到水塘抽样算法，就是解决如何在长度未知的序列（数据流）中随机选择一个元素的数学技巧。
+        当你遇到第 i 个元素时，应该有 1/i 的概率选择该元素，1 - 1/i 的概率保持原有的选择。
+        """
+        count, res = 0, -1
+        for i in range(len(self.nums)):
+            if self.nums[i] != target:
+                continue
+            count += 1
+            # 随机生成的概率等于1的情况下就是我们选择遇到第i个元素为概率 1/i 的情况
+            if self.rand.randint(1, count) == 1:
+                res = i
+        return res
+
+
+nums = [1, 2, 3, 3, 3]
+obj = Solution2(nums)
+print(obj.pick(3))
+print(obj.pick(1))
+print(obj.pick(3))
+
